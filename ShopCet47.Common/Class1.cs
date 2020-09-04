d0fa7490bc11eaa8c73f52a0a6bb2f34192bec4f
Williker Carmo <carmo.williker@gmail.com>
2020-09-04 21:24:43 +0100

    Init web project and library

---
+++ ShopCet47.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopCet47.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ ShopCet47.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopCet47.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
