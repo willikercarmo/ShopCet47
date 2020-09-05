28ca8175b4ed382c0d0eb3b902e86f436e3dca18
Williker Carmo <carmo.williker@gmail.com>
2020-09-05 11:49:59 +0100

    Connection Database

---
+++ ShopCet47.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCet47.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCet47.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
