4ba4e7a4635659130237fee08bd8c25aefac2fbc
Williker Carmo <carmo.williker@gmail.com>
2020-10-07 18:49:56 +0100

    Add and Edit Images

--- ShopCet47.Web/Controllers/ProductsController.cs
+++ ShopCet47.Web/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using ShopCet47.Web.Data;
 using ShopCet47.Web.Data.Entities;
 using ShopCet47.Web.Data.Repositories;
 using ShopCet47.Web.Helpers;
+using ShopCet47.Web.Models;
 
 namespace ShopCet47.Web.Controllers
 {
@@ -59,17 +61,53 @@ namespace ShopCet47.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Price,ImageUrl,LastPurchase,LastSale,IsAvailable,Stock")] Product product)
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,ImageFile,LastPurchase,LastSale,IsAvailable,Stock")] ProductViewModel view)
         {
             if (ModelState.IsValid)
             {
+
+                var path = string.Empty;
+
+                if(view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot\\images\\Products",
+                        view.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                }
+
+                var product = this.ToProduct(view, path);
+
                 //TODO: Mudar para o user que depois tiver logado
                 product.User = await _userHelper.GetUserByEmailAsync("williker.do.carmo@formandos.cinel.pt");
 
                 await _productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(view);
+        }
+
+        private Product ToProduct(ProductViewModel view, string path)
+        {
+            return new Product
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                IsAvailable = view.IsAvailable,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User
+            };
         }
 
         // GET: Products/Edit/5
@@ -85,7 +123,27 @@ namespace ShopCet47.Web.Controllers
             {
                 return NotFound();
             }
-            return View(product);
+            
+
+            var view = this.ToProductViewModel(product);
+
+            return View(view);
+        }
+
+        private ProductViewModel ToProductViewModel(Product product)
+        {
+            return new ProductViewModel
+            {
+                Id = product.Id,
+                ImageUrl = product.ImageUrl,
+                IsAvailable = product.IsAvailable,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                User = product.User
+            };
         }
 
         // POST: Products/Edit/5
@@ -93,9 +151,9 @@ namespace ShopCet47.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,ImageUrl,LastPurchase,LastSale,IsAvailable,Stock")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,ImageFile,LastPurchase,LastSale,IsAvailable,Stock")] ProductViewModel view)
         {
-            if (id != product.Id)
+            if (id != view.Id)
             {
                 return NotFound();
             }
@@ -104,13 +162,32 @@ namespace ShopCet47.Web.Controllers
             {
                 try
                 {
+                    var path = view.ImageUrl;
+
+                    if (view.ImageFile != null && view.ImageFile.Length > 0)
+                    {
+                        path = Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "wwwroot\\images\\Products",
+                            view.ImageFile.FileName);
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await view.ImageFile.CopyToAsync(stream);
+                        }
+
+                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                    }
+
+                    var product = this.ToProduct(view, path);
+
                     //TODO: Mudar para o user que depois tiver logado
                     await _productRepository.UpdateAsync(product);
 
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!await _productRepository.ExistAsync(product.Id))
+                    if (!await _productRepository.ExistAsync(view.Id))
                     {
                         return NotFound();
                     }
@@ -121,7 +198,7 @@ namespace ShopCet47.Web.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(view);
         }
 
         // GET: Products/Delete/5
---
+++ ShopCet47.Web/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopCet47.Web.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using ShopCet47.Web.Data.Entities;
+    using System.ComponentModel.DataAnnotations;
+    public class ProductViewModel : Product
+    {
+        [Display(Name="Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
